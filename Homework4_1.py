db64921f820e2172e60420828320dba67fc23c69
BogdanSahno <156195789+BogdanSahno@users.noreply.github.com>
2024-02-03 11:36:02 +0100

    Add files via upload

---
+++ Homework4_1.py
@@ -0,0 +1,21 @@
+def total_salary(path:str):
+    
+    total_salary = 0
+    total_employers = 0
+
+    try:
+        with open(path, "r", encoding="utf-8") as file:
+            for lines in file.readlines():
+                total_employers += 1
+                
+                content_line = lines.strip().split(",")
+                total_salary += int(content_line [1])
+                
+        return total_salary, (total_salary / total_employers)
+    
+    except Exception as errors_file:
+        print (errors_file)
+    
+
+total, average = total_salary("salary.txt")
+print(f"Загальна сума зарплати: {total}, Середня зарплата: {average}")
